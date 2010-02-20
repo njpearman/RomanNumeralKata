d1f28e62d33da000b6bc5caf16769df96599391e
Neill Pearman <neill@stackulous.com>
2010-02-20 00:23:40 +0000

    Made OCAML file valid.

--- lib/ocaml/roman_numerals_kata.ml
+++ lib/ocaml/roman_numerals_kata.ml
@@ -1,7 +1,9 @@
 (* First step in making an OCAML Roman ?Numeral converter *)
-add_i numerals value = 
-  		match value with
-  		| 0 -> numerals
-  		| x -> add_i (numerals ^ "I") (value-1) in
-  	add_i "" value
-  ;;
+let rec add_i numerals value =
+	match value with
+  	| 0 -> numerals
+  	| x -> add_i (numerals ^ "I") (value-1)
+;;
+let convert value =
+	add_i "" value
+;;
