39043369c698ea3242b1784d3697d91d509a7302
Neill Pearman <neill@stackulous.com>
2010-02-20 00:54:19 +0000

    More OCAML stuff; no unit tests, though.

--- lib/ocaml/roman_numerals_kata.ml
+++ lib/ocaml/roman_numerals_kata.ml
@@ -1,9 +1,21 @@
 (* First step in making an OCAML Roman ?Numeral converter *)
+
 let rec add_i numerals value =
 	match value with
   	| 0 -> numerals
   	| x -> add_i (numerals ^ "I") (value-1)
 ;;
-let convert value =
-	add_i "" value
+
+let add_numerals total value =
+	match value with
+	| 9 -> "IX"
+	| 5 -> "V"
+	| 4 -> "IV"
+	| x -> add_i "" value
 ;;
+
+let convert value =
+	print_string ((add_numerals "" value) ^ "\n") 
+;;
+
+convert (int_of_string Sys.argv.(1));;
