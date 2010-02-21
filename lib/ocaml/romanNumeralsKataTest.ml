de6a4ae15021c98102846f14e5c55726524af25f
Neill Pearman <neill@stackulous.com>
2010-02-21 21:07:37 +0000

    Added test cases up to 10.

--- lib/ocaml/romanNumeralsKataTest.ml
+++ lib/ocaml/romanNumeralsKataTest.ml
@@ -7,6 +7,24 @@ let test_fixture = "Roman numerals kata" >:::
 [
 	"should return I for 1" >:: ( fun () -> 
 		assert_equal "I" (RomanNumeralsKata.to_numerals 1) );
+	"should return II for 2" >:: ( fun () -> 
+		assert_equal "II" (RomanNumeralsKata.to_numerals 2) );
+	"should return III for 3" >:: ( fun () -> 
+		assert_equal "III" (RomanNumeralsKata.to_numerals 3) );
+	"should return IV for 4" >:: ( fun () -> 
+		assert_equal "IV" (RomanNumeralsKata.to_numerals 4) );
+	"should return V for 5" >:: ( fun () -> 
+		assert_equal "V" (RomanNumeralsKata.to_numerals 5) );
+	"should return VI for 6" >:: ( fun () -> 
+		assert_equal "VI" (RomanNumeralsKata.to_numerals 6) );
+	"should return VII for 7" >:: ( fun () -> 
+		assert_equal "VII" (RomanNumeralsKata.to_numerals 7) );
+	"should return VIII for 8" >:: ( fun () -> 
+		assert_equal "VIII" (RomanNumeralsKata.to_numerals 8) );
+	"should return IX for 9" >:: ( fun () -> 
+		assert_equal "IX" (RomanNumeralsKata.to_numerals 9) );
+	"should return X for 10" >:: ( fun () -> 
+		assert_equal "X" (RomanNumeralsKata.to_numerals 10) );
 ]
 
 (* Run the tests! *)
