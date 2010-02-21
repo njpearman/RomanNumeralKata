7f00857103fca9923d5c62a4829e70c277c8121f
Neill Pearman <neill@stackulous.com>
2010-02-21 20:57:52 +0000

    Changed test file name in compile script.

--- lib/ocaml/compile_all.sh
+++ lib/ocaml/compile_all.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 ocamlc -o kata.run romanNumeralsKata.ml 
-ocamlfind ocamlc -c -linkpkg -package oUnit roman_numerals_kata_test.ml 
-ocamlfind ocamlc -o tests.run -linkpkg -package oUnit romanNumeralsKata.cmo roman_numerals_kata_test.cmo
+ocamlfind ocamlc -c -linkpkg -package oUnit romanNumeralsKataTest.ml 
+ocamlfind ocamlc -o tests.run -linkpkg -package oUnit romanNumeralsKata.cmo romanNumeralsKataTest.cmo
